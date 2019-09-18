46eea77071af29e5d04088272dd2fc80daad4327
liuqiuyue <miryue@163.com>
2019-09-18 13:49:46 +0800

    test

---
+++ src/test.js
@@ -0,0 +1,17 @@
+// import React, { Component } from 'react'
+
+// export default class test extends Component {
+//   render() {
+//     return (
+//       <div>
+        
+//       </div>
+//     )
+//   }
+
+function  test({params}) {
+  return <div>{params}</div>
+}
+ 
+
+  // 简单组件可以使用函数代替
